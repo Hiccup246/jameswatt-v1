8c578a2d57700d3468e19e41dce6f1c86b9b5980
James Watt <james.watt529@outlook.com>
2021-10-30 19:52:58 +1300

    update icons

--- src/components/theme-toggle/theme-toggle.js
+++ src/components/theme-toggle/theme-toggle.js
@@ -1,43 +1,9 @@
 import * as React from 'react'
-import moon from '../../assets/moon-regular.svg'
-import sun from '../../assets/sun-regular.svg'
+import moon from '../../assets/fa-moon-regular.svg'
+import sun from '../../assets/icons8-sun-regular.svg'
+
 import './theme-toggle.css'
 import { ThemeContext } from '../theme-context/theme-context';
-
-
-// class ThemeToggle extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { theme: 'light' };
-//     this.toggleTheme = this.toggleTheme.bind(this);
-//     this.getIcon = this.getIcon.bind(this);
-//   }
-
-//   toggleTheme() {
-//     this.setState({ theme: this.state.theme == 'light' ? 'dark' : 'light' });
-//   }
-
-//   getIcon() {
-//     if (this.state.theme == 'light') {
-//       return(<img className="theme-toggle__theme-icon" src={moon} alt='Moon'/>)
-//     } else {
-//       return(<img className="theme-toggle__theme-icon" src={sun} alt='Sun'/>)
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className='theme-toggle' onClick={this.toggleTheme}>
-//       { this.getIcon() }
-//       {/* <img className="" src={sun} alt='Sun'/> */}
-//     </div>
-//     )
-//   }
-// }
-
-
-
-// export default ThemeToggle
 
 function getSun() {
   return(<img className="theme-toggle__theme-icon" src={sun} alt='Sun'/>)
