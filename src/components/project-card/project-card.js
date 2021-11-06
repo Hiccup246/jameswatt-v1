9c469bfa4b0b28de768e73104a61f4f8da03f003
James Watt <james.watt529@outlook.com>
2021-11-06 19:41:24 +1300

    complete splitting of projects into projects card

--- src/components/project-card/project-card.js
+++ src/components/project-card/project-card.js
@@ -1,9 +1,65 @@
 import * as React from 'react'
 
+import linkIcon from '../../assets/link-solid.svg'
+
 import './project-card.css'
 
-const ProjectCard = ({ image, description, link, linkText }) => (
-  <div class="projects__card"></div>
+const Icon = () => {
+  function rotateIcon(e) {
+    e.preventDefault()
+
+    const currentTarget = e.currentTarget
+
+    const card = currentTarget.closest('.projects__card')
+
+    card.getElementsByClassName('icon')[0].classList.toggle('icon--checked')
+
+    card
+      .getElementsByClassName('projects__card-description')[0]
+      .classList.toggle('projects__card-description--checked')
+    card
+      .getElementsByClassName('projects__card-body')[0]
+      .classList.toggle('projects__card-body--checked')
+  }
+
+  return (
+    <div class="icon" onClick={rotateIcon}>
+      <svg
+        class="tile-icon-alt"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path d="M17.25,8.51H11.5V2.75A1.5,1.5,0,0,0,10,1.25h0a1.5,1.5,0,0,0-1.5,1.5V8.5H2.75a1.5,1.5,0,0,0,0,3H8.5v5.75a1.5,1.5,0,0,0,1.5,1.5h0a1.5,1.5,0,0,0,1.5-1.5V11.5h5.75a1.5,1.5,0,0,0,0-3Z"></path>
+      </svg>
+    </div>
+  )
+}
+
+const ProjectCard = ({ imageSrc, description, link, linkText, key }) => (
+  <div class="projects__card">
+    <div class="projects__card-body">
+      <img
+        class="projects__card-picture"
+        src={imageSrc}
+        alt="Cryto Zombies Logo"
+      />
+
+      <div class="projects__card-footer">
+        <div class="projects__card-link">
+          <a href={link}>{linkText}</a>
+          <img src={linkIcon} alt="Link Icon" />
+        </div>
+      </div>
+    </div>
+
+    <div class="projects__body-overlay">
+      <div class="projects__icon-wrapper">
+        <Icon />
+      </div>
+
+      <div class="projects__card-description">{description}</div>
+    </div>
+  </div>
 )
 
 export default ProjectCard
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,72 +1,27 @@
 import * as React from 'react'
 
-import link from '../../assets/link-solid.svg'
 import cryptozombies from '../../assets/project-images/crypto-zombies.png'
 import goblockchain from '../../assets/project-images/go-blockchain.png'
 import './projects.css'
+import ProjectCard from '../project-card/project-card'
 
-const Icon = () => {
-  function rotateIcon(e) {
-    e.preventDefault()
-    e.currentTarget.classList.toggle('icon--checked')
-    document
-      .getElementsByClassName('projects__card-description')[0]
-      .classList.toggle('projects__card-description--checked')
-
-    document
-      .getElementsByClassName('projects__card-body')[0]
-      .classList.toggle('projects__card-body--checked')
-  }
-
-  return (
-    <div class="icon" onClick={rotateIcon}>
-      <svg
-        class="tile-icon-alt"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path d="M17.25,8.51H11.5V2.75A1.5,1.5,0,0,0,10,1.25h0a1.5,1.5,0,0,0-1.5,1.5V8.5H2.75a1.5,1.5,0,0,0,0,3H8.5v5.75a1.5,1.5,0,0,0,1.5,1.5h0a1.5,1.5,0,0,0,1.5-1.5V11.5h5.75a1.5,1.5,0,0,0,0-3Z"></path>
-      </svg>
-    </div>
-  )
-}
 const Projects = () => (
   <div class="projects">
     <h4>Projects</h4>
 
     <div class="projects__slider">
-      <div class="projects__card">
-        <div class="projects__card-body">
-          <img
-            class="projects__card-picture"
-            src={cryptozombies}
-            alt="Cryto Zombies Logo"
-          />
-
-          <div class="projects__card-footer">
-            <div class="projects__card-link">
-              <a href="https://cryptozombies.io/">Crypto Zombies</a>
-              <img src={link} alt="Link Icon" />
-            </div>
-          </div>
-        </div>
-
-        <div class="projects__body-overlay">
-          <div class="icon-wrapper">
-            <Icon />
-          </div>
-
-          <div class="projects__card-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </div>
-        </div>
-      </div>
-
-      <div class="projects__card"></div>
-      <div class="projects__card"></div>
-      <div class="projects__card"></div>
-      <div class="projects__card"></div>
+      <ProjectCard
+        imageSrc={cryptozombies}
+        description="I like to eat cheese"
+        link="https://cryptozombies.io/"
+        linkText="Crypto Zombies"
+      />
+      <ProjectCard
+        imageSrc={goblockchain}
+        description="I like to eat cheese, as well as, meat"
+        link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
+        linkText="Go Blockchain"
+      />
     </div>
   </div>
 )
