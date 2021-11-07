ca3ebec2fa3ff5e42df14c5085daa4db3a42fae3
James Watt <james.watt529@outlook.com>
2021-11-07 19:20:30 +1300

    complete rough draft of programming history table

--- src/components/programming-history/programming-history.js
+++ src/components/programming-history/programming-history.js
@@ -5,6 +5,35 @@ import './programming-history.css'
 const ProgrammingHistory = () => (
   <div class="programming-history">
     <h4>Programming History</h4>
+
+    <div class="tab-component">
+      <div class="tab-menu">
+        <div class="tab-item">AplyiD</div>
+        <div class="tab-item">Halter</div>
+        <div class="tab-item">Hypebeat</div>
+        <div class="tab-item">BNZ</div>
+        <div class="tab-item">VUW</div>
+        <div class="selected-tab-item"></div>
+      </div>
+
+      <div class="tab-content">
+        <div class="tab-panel">
+          <h3>
+            <span>Software Engineer</span>
+            <span> @ AplyiD</span>
+          </h3>
+          <p>xxx - xxx</p>
+          <div>
+            <ul class="experience-points">
+              <li>xxxx</li>
+              <li>xxxx</li>
+              <li>xxxx</li>
+              <li>xxxx</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
