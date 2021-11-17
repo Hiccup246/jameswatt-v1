e5f85214eb81c58bdac20637862a570b7027648d
James Watt <james.watt529@outlook.com>
2021-11-17 23:39:48 +1300

    complete show/hide

--- src/components/bookshelf/bookshelf.js
+++ src/components/bookshelf/bookshelf.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 
 import bookDark from '../../assets/book-dark.gif'
 import bookLight from '../../assets/book-light.gif'
-
 import Completed from '../../assets/svg/clipboard-check.svg'
 import { ThemeContext } from '../theme-context/theme-context'
 
@@ -13,147 +12,153 @@ const Bookshelf = () => {
 
   const books = [
     {
-      name: "On Having No Head",
-      author: "Tolkien",
-      genre: "Fantasy",
-      status: "Read",
-      dateCompleted: ""
+      name: 'Rage',
+      author: 'Bob Woodward',
+      genre: 'Politics',
+      status: 'Reading',
+      dateCompleted: '',
     },
     {
-      name: "The Lord of the Rings The Return of the King",
-      author: "J.R.R. Tolkien",
-      genre: "Fantasy",
-      status: "Read",
-      dateCompleted: ""
+      name: 'On Having No Head',
+      author: 'Tolkien',
+      genre: 'Fantasy',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "The Lord of the Rings The Two Towers",
-      author: "J.R.R. Tolkien",
-      genre: "Fantasy",
-      status: "Read",
-      dateCompleted: ""
+      name: 'The Lord of the Rings The Return of the King',
+      author: 'J.R.R. Tolkien',
+      genre: 'Fantasy',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "Lord of the Rings the Fellowship of the Ring",
-      author: "J.R.R. Tolkien",
-      genre: "Fantasy",
-      status: "Read",
-      dateCompleted: ""
+      name: 'The Lord of the Rings The Two Towers',
+      author: 'J.R.R. Tolkien',
+      genre: 'Fantasy',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "The Hobbit",
-      author: "J.R.R. Tolkien",
-      genre: "Fantasy",
-      status: "Read",
-      dateCompleted: ""
+      name: 'Lord of the Rings the Fellowship of the Ring',
+      author: 'J.R.R. Tolkien',
+      genre: 'Fantasy',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "12 Rules for Life: An Antidote to Chaos",
-      author: "Jordan B. Peterson",
-      genre: "Self Help",
-      status: "Read",
-      dateCompleted: ""
+      name: 'The Hobbit',
+      author: 'J.R.R. Tolkien',
+      genre: 'Fantasy',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "Sapiens: A Brief History of Humankind",
-      author: "Yuval Noah Harari",
-      genre: "History",
-      status: "Read",
-      dateCompleted: ""
+      name: '12 Rules for Life: An Antidote to Chaos',
+      author: 'Jordan B. Peterson',
+      genre: 'Self Help',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "Propaganda",
-      author: "Edward L. Bernays",
-      genre: "Psychology",
-      status: "Read",
-      dateCompleted: ""
+      name: 'Sapiens: A Brief History of Humankind',
+      author: 'Yuval Noah Harari',
+      genre: 'History',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "Fear: Trump in the White House",
-      author: "Bob Woodward",
-      genre: "Politics",
-      status: "Read",
-      dateCompleted: ""
+      name: 'Propaganda',
+      author: 'Edward L. Bernays',
+      genre: 'Psychology',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "Animal Farm",
-      author: "George Orwell",
-      genre: "Politics",
-      status: "Read",
-      dateCompleted: ""
+      name: 'Fear: Trump in the White House',
+      author: 'Bob Woodward',
+      genre: 'Politics',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "1984",
-      author: "George Orwell",
-      genre: "Politics",
-      status: "Read",
-      dateCompleted: ""
+      name: 'Animal Farm',
+      author: 'George Orwell',
+      genre: 'Politics',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "Manufacturing Consent",
-      author: "Edward S. Herman, Noam Chomsky",
-      genre: "Politics",
-      status: "Read",
-      dateCompleted: ""
+      name: '1984',
+      author: 'George Orwell',
+      genre: 'Politics',
+      status: 'Read',
+      dateCompleted: '',
     },
     {
-      name: "The Power of Now",
-      author: "Eckhart Tolle",
-      genre: "Spirituality",
-      status: "Read",
-      dateCompleted: ""
-    }
+      name: 'Manufacturing Consent',
+      author: 'Edward S. Herman, Noam Chomsky',
+      genre: 'Politics',
+      status: 'Read',
+      dateCompleted: '',
+    },
+    {
+      name: 'The Power of Now',
+      author: 'Eckhart Tolle',
+      genre: 'Spirituality',
+      status: 'Read',
+      dateCompleted: '',
+    },
   ]
 
-  const showHideBooks = (show) => {
+  const showHideBooks = () => {
     const shortBooksLength = 4
     const totalBooks = books.length
 
     for (let step = shortBooksLength; step < totalBooks; step++) {
-      if (show) {
-        document.getElementById("book-"+step).style.display = 'none'
-      } else {
-        document.getElementById("book-"+step).style.display = 'block'
-      }
+      document.getElementById('book-' + step).classList.toggle('inactive')
     }
 
-    if(show) {
-      document.getElementsByClassName("bookshelf__show-more")[0].innerHTML = "Show More"
+    if (
+      document.getElementsByClassName('bookshelf__show-more')[0].innerHTML ===
+      'Show More'
+    ) {
+      document.getElementsByClassName('bookshelf__show-more')[0].innerHTML =
+        'Show Less'
     } else {
-      document.getElementsByClassName("bookshelf__show-more")[0].innerHTML = "Show Less"
-    }
-  }
-
-  const booksShowing = () => {
-    if (document.getElementById("book-6") && document.getElementById("book-6").style.display === "auto") {
-      return true;
-    } else {
-      return false;
+      document.getElementsByClassName('bookshelf__show-more')[0].innerHTML =
+        'Show More'
     }
   }
 
   const getBookStatusIcon = (status) => {
-    if (status === "Read") {
-        return <Completed className='bookshelf__completed' />
+    if (status === 'Read') {
+      return <Completed className="bookshelf__completed" />
     } else {
-      return  <img
-                  className="bookshelf__reading-icon"
-                  src={siteTheme === 'dark' ? bookDark : bookLight}
-                  alt="Currently reading"
-                />
+      return (
+        <img
+          className="bookshelf__reading-icon"
+          src={siteTheme === 'dark' ? bookDark : bookLight}
+          alt="Currently reading"
+        />
+      )
     }
   }
 
   const booksList = books.map((book, index) => {
     return (
-      <tr key={index} id={"book-"+index}>
+      <tr
+        key={index}
+        id={'book-' + index}
+        className={index > 3 ? 'inactive' : ''}
+      >
         <td className="bookshelf__name-cell">
           <h4>{book.name}</h4>
           <div>{book.author}</div>
         </td>
         <td className="bookshelf__genre-cell">{book.genre}</td>
         <td className="bookshelf__status-cell">
-          { getBookStatusIcon(book.status) }
+          {getBookStatusIcon(book.status)}
         </td>
       </tr>
     )
@@ -174,12 +179,12 @@ const Bookshelf = () => {
               <th className="bookshelf__status-header">Status</th>
             </tr>
           </thead>
-          <tbody>
-            { booksList }
-          </tbody>
+          <tbody>{booksList}</tbody>
         </table>
 
-        <div className="bookshelf__show-more" onClick={() => showHideBooks(booksShowing() ? true : false)}>Show More</div>
+        <div className="bookshelf__show-more" onClick={() => showHideBooks()}>
+          Show More
+        </div>
       </div>
     </div>
   )
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -12,48 +12,13 @@ const Projects = () => (
     <div className="projects__slider">
       <ProjectCard
         imageSrc={cryptozombies}
-        description="I like to eat cheese"
+        description="Crypto zombies is a solidity programming course (the smart contracts language notably used by etherium). I completed the beginner, intermediate and advanced solidity programming tutorials which introduced concepts such as oracles and smart contract design."
         link="https://cryptozombies.io/"
         linkText="Crypto Zombies"
       />
       <ProjectCard
         imageSrc={goblockchain}
-        description="I like to eat cheese, as well as, meat"
-        link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
-        linkText="Go Blockchain"
-      />
-
-      <ProjectCard
-        imageSrc={goblockchain}
-        description="I like to eat cheese, as well as, meat"
-        link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
-        linkText="Go Blockchain"
-      />
-
-      <ProjectCard
-        imageSrc={goblockchain}
-        description="I like to eat cheese, as well as, meat"
-        link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
-        linkText="Go Blockchain"
-      />
-
-      <ProjectCard
-        imageSrc={goblockchain}
-        description="I like to eat cheese, as well as, meat"
-        link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
-        linkText="Go Blockchain"
-      />
-
-      <ProjectCard
-        imageSrc={goblockchain}
-        description="I like to eat cheese, as well as, meat"
-        link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
-        linkText="Go Blockchain"
-      />
-
-      <ProjectCard
-        imageSrc={goblockchain}
-        description="I like to eat cheese, as well as, meat"
+        description="I completed a 10 part youtube tutorial which involved re creating key aspects of the bitocin blockchain such as transactions, encryption, wallets and mining using the functional programming language Go."
         link="https://www.youtube.com/watch?v=mYlHT9bB6OE"
         linkText="Go Blockchain"
       />
