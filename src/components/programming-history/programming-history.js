c7582daece41d5f4cb47a2953c67ac2258cab8b3
James Watt <james.watt529@outlook.com>
2021-11-18 21:15:50 +1300

    fill out employment history

--- src/components/programming-history/programming-history.js
+++ src/components/programming-history/programming-history.js
@@ -6,10 +6,22 @@ const experiences = [
   {
     role: 'Software Engineer',
     company: 'AplyiD',
-    dateRange: '2019/04/02 - Present',
+    dateRange: '2020/01 - Present',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      'APLYiD is a trusted & accredited digital ID solution (AML + KYC) designed to streamline your customer onboarding with a first-class user experience.',
     bullets: [
+      'Joined the company as the second developer and 13th employee and helped grow the product using agile practices into a strong team of 7 and an organisation  ',
+      'Worked as a full stack developer creating end to end produc',
+    ],
+  },
+  {
+    role: 'Full Stack Developer',
+    company: 'Hypebeat',
+    dateRange: '2019/04 - 2019/09',
+    description:
+      'A small social music application to aid artists in optimising their reach.',
+    bullets: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     ],
@@ -17,45 +29,37 @@ const experiences = [
   {
     role: 'Intern Node Developer',
     company: 'Halter',
-    dateRange: '2018/03/23 - 2019/04/02',
-    description: '',
+    dateRange: '2018/11 - 2019/02',
+    description:
+      'Halter is a solar-powered GPS enabled smart collar, which guides cows around a farm using sound and vibrations, allowing farmers to automate herd movements and create virtual fences.',
     bullets: [
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    ],
-  },
-  {
-    role: 'Full Stack Developer',
-    company: 'Hypebeat',
-    dateRange: '2017/01/03 - 2018/03/23',
-    description: '',
-    bullets: [
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      'Worked as part of the core agile development software team helping to develop the companies cloud infrastructure',
+      'While developing the cloud infrastructure I utilised technologises such as Nest.JS, Swagger, OpenAPI and AWS',
+      'AWS development centered around data pipeline creation where data from IoT devices was listened for, captured, transformed and sent to long term storage',
     ],
   },
   {
     role: 'Front End Developer',
     company: 'BNZ',
-    dateRange: '2017/01/03 - 2018/03/23',
-    description: '',
+    dateRange: '2017/11 - 2018/02',
+    description: "Bank of New Zealand is one of New Zealand's big four banks.",
     bullets: [
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      'Helped to develop a series of internal facing front end components e.g. Date pickers, dropdowns etc',
+      'Utilised web technologies and tools such as React, CSS, HTML, Node and Javascript',
+      'Worked closely with both experienced designers and developers in a small agile team which required lots of communication',
     ],
   },
   {
-    role: 'Student',
+    role: 'Student & Tutor',
     company: 'VUW',
-    dateRange: '2017/01/03 - 2018/03/23',
-    description: '',
+    dateRange: '2016 - 2020',
+    description:
+      'Victoria University of Wellington is a university in Wellington, New Zealand. The university has a strong software engineering school and an impressive reputation regarding artifical intelligence research.',
     bullets: [
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      'Graduated with a Bachelors of Engineering with first class honours majoring in software',
+      'Key study interests included artificial intelligence, software ethics and organisational culture',
+      'Completed a year long thesis which involved developing a series of IoT connected radios. The purpose of the radios was to be able to track animal wihin a local bird park. The idea was to setup a series of radios throughout the birdpark and then attach some to the birds themeselves. The radios could then connect to each other and using novel localisation algorithms pin point the location of the birds within the parks dense bush.',
+      'Worked as a computer science tutor helping students to understand algorithms and data structures',
     ],
   },
 ]
