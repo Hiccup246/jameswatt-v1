25e1a876f3882d21cedb1d7a8460cfb2c7834e35
James Watt <james.watt529@outlook.com>
2022-04-21 19:01:22 +0100

    make project cards use buttons

--- src/components/content-toggle/content-toggle.js
+++ src/components/content-toggle/content-toggle.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import './content-toggle.css'
 
 const ContentToggle = ({ onClickCallback }) => (
-  <div className="icon" onClick={onClickCallback}>
+  <button className="icon" onClick={onClickCallback}>
     <svg
       className="tile-icon-alt"
       xmlns="http://www.w3.org/2000/svg"
@@ -11,7 +11,7 @@ const ContentToggle = ({ onClickCallback }) => (
     >
       <path d="M17.25,8.51H11.5V2.75A1.5,1.5,0,0,0,10,1.25h0a1.5,1.5,0,0,0-1.5,1.5V8.5H2.75a1.5,1.5,0,0,0,0,3H8.5v5.75a1.5,1.5,0,0,0,1.5,1.5h0a1.5,1.5,0,0,0,1.5-1.5V11.5h5.75a1.5,1.5,0,0,0,0-3Z"></path>
     </svg>
-  </div>
+  </button>
 )
 
 export default ContentToggle
