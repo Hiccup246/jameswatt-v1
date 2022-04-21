611d0518781978eeed6bf2f9de069ebc30a30284
James Watt <james.watt529@outlook.com>
2022-04-21 19:27:53 +0100

    add bolding to programming history

--- src/constants/programming-experiences.js
+++ src/constants/programming-experiences.js
@@ -1,3 +1,5 @@
+import * as React from 'react'
+
 export const PROGRAMMING_EXPERIENCES = [
   {
     role: 'Full Stack Developer',
@@ -6,9 +8,24 @@ export const PROGRAMMING_EXPERIENCES = [
     description:
       'APLYiD is a trusted & accredited digital ID solution (AML + KYC) designed to streamline your customer onboarding with a first-class user experience',
     bullets: [
-      'Currently working as a full stack developer utilising Vue.JS, Angular.JS and Ruby on Rails to create AML and financial products from inception and design to deployment and maintenance',
-      'Joined the company as the second developer and 13th employee. Helped grow the product, team and organisation using agile practices into a strong team of 7 and an organisation of over 50 people',
-      'Worked closley with the founders of the company, key sales staff, designers and customer support to design products from the ground up',
+      <p>
+        Currently working as a full stack developer utilising <b>Vue.JS</b>,
+        Angular.JS and <b>Ruby on Rails</b> to create KYC/AML and financial
+        onboarding products from inception and design to deployment and
+        maintenance
+      </p>,
+      <p>
+        Successfully <b>lead major development initiatives</b> to grow the
+        product and company. This resulted in our development team of 2 and
+        organisation of 13 to become a strong development team of 7 and an
+        organisation of over 50 spanning across 3 timezones.
+      </p>,
+      <p>
+        Collaborated closley with the founders of the company, key sales staff,
+        developers, designers, third party vendors and customer support to{' '}
+        navigate complex AML regulation and data policies to produce slick
+        onboarding products.
+      </p>,
     ],
   },
   {
@@ -18,7 +35,11 @@ export const PROGRAMMING_EXPERIENCES = [
     description:
       'A social music application to aid artists in optimising their reach and influence using artifical intelligence',
     bullets: [
-      'Worked closley with the founders of the company in an agile team to develop and expand the back-end of the main product using Koa.JS (JavaScript + TypeScript)',
+      <p>
+        Worked closley with the founders of the company in an agile team to
+        develop and expand the back-end of the main product using <b>Koa.JS</b>{' '}
+        which included Javascript, Typescript, HTML and CSS
+      </p>,
       'Participated in key strategic meetings regarding business strategy and product design',
     ],
   },
@@ -30,8 +51,15 @@ export const PROGRAMMING_EXPERIENCES = [
       'Halter is a solar-powered GPS enabled smart collar, which guides cows around a farm using sound and vibrations, allowing farmers to automate herd movements and create virtual fences',
     bullets: [
       'Worked in two high performing agile teams which developed the minimal viable product of the company under tight product deadlines',
-      'Developed and expanded the back-end and cloud infrastructure of the main product using Nest.JS (JavaScript + TypeScript), AWS, Swagger, OpenAPI and Docker',
-      'Developed the front-end of the company by creating various internal support tools using the React.JS framework ',
+      <p>
+        Developed and expanded the back-end and <b>cloud infrastructure</b> of
+        the main product using Nest.JS (JavaScript + TypeScript), AWS, Swagger,
+        OpenAPI and Docker
+      </p>,
+      <p>
+        Developed the front-end of the company by creating various internal
+        support tools using the <b>React.JS</b> framework
+      </p>,
     ],
   },
   {
@@ -40,8 +68,15 @@ export const PROGRAMMING_EXPERIENCES = [
     dateRange: '2017/11 - 2018/02',
     description: "Bank of New Zealand is one of New Zealand's big four banks",
     bullets: [
-      'Developed front-end software using the React.JS framework alongside technologies such as JavaScript, HTML, CSS, JSS and Node.JS to create various visual frontend components',
-      'Worked closely with experienced product designers and owners in a small agile team requiring tight communication',
+      <p>
+        Developed front-end software using the <b>React.JS</b> framework
+        alongside technologies such as JavaScript, HTML, CSS, JSS and Node.JS to
+        create various visual frontend components
+      </p>,
+      <p>
+        Worked closely with experienced <b>product designers and owners</b> in a
+        small agile team requiring tight communication
+      </p>,
       'Partially worked in an agile team to test the banks internal help and support system via automated unit tests and test plans using XMind and Postman',
     ],
   },
@@ -53,8 +88,15 @@ export const PROGRAMMING_EXPERIENCES = [
       'Victoria University of Wellington is a university in Wellington, New Zealand. The university has a strong software engineering school and an impressive reputation regarding artifical intelligence research',
     bullets: [
       'Graduated with a Bachelor of Engineering in Software Engineering with first class honours',
-      'Key study interests included artificial intelligence, software ethics and organisational culture',
-      'Completed a year long thesis which involved developing a series of IoT connected radios for the purpose of tracking birds within dense native bush',
+      <p>
+        Key study interests included <b>artificial intelligence</b>, software
+        ethics and <b>organisational culture</b>
+      </p>,
+      <p>
+        Completed a year long thesis which involved developing a series of{' '}
+        <b>IoT</b> connected radios for the purpose of tracking birds within
+        dense native bush
+      </p>,
       'Worked as a computer science tutor helping students to understand algorithms and data structures',
     ],
   },
