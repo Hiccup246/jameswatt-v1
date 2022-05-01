3bac37d094de3d352bbf6499585c914a0f139387
James Watt <james.watt529@outlook.com>
2022-05-01 13:04:59 +0100

    add template for blog home

---
+++ src/components/blog-home/blog-home.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import './blog-home.css'
+
+const BlogHome = () => (
+  <div className="blog-home">
+    <p>Testing the rendering of blog home</p>
+  </div>
+)
+
+export default BlogHome
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,13 +2,15 @@ import * as React from 'react'
 
 import BaseLayout from '../components/base-layout/base-layout'
 import '../styles/global.css'
+import BlogHome from '../components/blog-home/blog-home'
 import Seo from '../components/seo/seo'
 import { ThemeProvider } from '../components/theme-context/theme-context'
 
 const Blog = () => (
   <ThemeProvider>
     <BaseLayout id="blog">
-      <Seo title="James Watt" />
+      <Seo title="James Watt Blog" />
+      <BlogHome />
     </BaseLayout>
   </ThemeProvider>
 )
