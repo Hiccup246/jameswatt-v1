ee29dcc31fcd1c84311fade5b200ab33c8d05f7d
James Watt <james.watt529@outlook.com>
2022-05-02 16:31:48 +0100

    update splugs and blog post formatting

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,60 +3,55 @@ import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import './blog-post.css'
 
-import BaseLayout from '../components/base-layout/base-layout'
-import { ThemeProvider } from '../components/theme-context/theme-context'
-
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const { previous, next } = data
 
   return (
-   <div>
-        <div className="blog-post-wrapper">
-          <article
-            className="blog-post"
-            itemScope
-            itemType="http://schema.org/Article"
-          >
-            <header className="blog-post__header">
-              <h1>{post.frontmatter.title}</h1>
-              <p>{post.frontmatter.date}</p>
-            </header>
-            <section
-              dangerouslySetInnerHTML={{ __html: post.html }}
-              itemProp="articleBody"
-            />
-            <hr />
-            <footer></footer>
-          </article>
-          <nav className="blog-post-nav">
-            <ul
-              style={{
-                display: `flex`,
-                flexWrap: `wrap`,
-                justifyContent: `space-between`,
-                listStyle: `none`,
-                padding: 0,
-              }}
-            >
-              <li>
-                {previous && (
-                  <Link to={previous.fields.slug} rel="prev">
-                    ← {previous.frontmatter.title}
-                  </Link>
-                )}
-              </li>
-              <li>
-                {next && (
-                  <Link to={next.fields.slug} rel="next">
-                    {next.frontmatter.title} →
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+    <div className="blog-post-wrapper">
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header className="blog-post__header">
+          <h1>{post.frontmatter.title}</h1>
+          <p>{post.frontmatter.date}</p>
+        </header>
+        <section
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          itemProp="articleBody"
+        />
+        <hr />
+        <footer></footer>
+      </article>
+      <nav className="blog-post-nav">
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
