fe74d8bf813534f750d132b3a7f3cb4c780c1cd0
James Watt <james.watt529@outlook.com>
2022-05-02 17:11:34 +0100

    add styling for no blog posts

--- src/components/blog-home/blog-home.js
+++ src/components/blog-home/blog-home.js
@@ -7,30 +7,38 @@ import './blog-home.css'
 const BlogHome = ({ data }) => {
   const posts = data.allMarkdownRemark.nodes
 
-  return (
-    <ol className="post-list">
-      {posts.map((post) => {
-        const postLink = post.fields.slug
-        const title = post.frontmatter.title || postLink
-        const date = post.frontmatter.date
+  if (posts.length === 0) {
+    return (
+      <p className="no-posts">
+        Subscribe to the RSS feed to get notified of new posts!
+      </p>
+    )
+  } else {
+    return (
+      <ol className="post-list">
+        {posts.map((post) => {
+          const postLink = post.fields.slug
+          const title = post.frontmatter.title || postLink
+          const date = post.frontmatter.date
 
-        return (
-          <li key={postLink} className="post-list__item">
-            <article itemType="http://schema.org/Article">
-              <header className="post-list__header">
-                <h1>
-                  <Link className="link--regular-font" to={postLink}>
-                    {title}
-                  </Link>
-                </h1>
-                <small>{date}</small>
-              </header>
-            </article>
-          </li>
-        )
-      })}
-    </ol>
-  )
+          return (
+            <li key={postLink} className="post-list__item">
+              <article itemType="http://schema.org/Article">
+                <header className="post-list__header">
+                  <h1>
+                    <Link className="link--regular-font" to={postLink}>
+                      {title}
+                    </Link>
+                  </h1>
+                  <small>{date}</small>
+                </header>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    )
+  }
 }
 
 export default BlogHome
