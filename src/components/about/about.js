d6072182b1b5062ddbeb911b5f3e4d852fe3f7d1
James Watt <james.watt529@outlook.com>
2022-05-21 20:22:20 +0100

    Update about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,52 +9,18 @@ const About = () => (
     </div>
     <div className="about__description-short selected">
       <p>
-        I'm a full stack software developer based out of London, England and an
-        xpat of New Zealand. I enjoy software development for its sometimes mind
-        bending mix of subjectiveness and objectivness where there is almost
-        never a sliver bullet. This has helped me develop extensive software
-        skill over 6 years of programming and 3 years of working in high
-        performing agile teams.
+        I'm a software engineer based out of London, England and an xpat of New
+        Zealand. I enjoy software engineering for its sometimes mind bending mix
+        of subjectiveness and objectivness where there is almost never a sliver
+        bullet. This has helped me develop extensive software skill over 6 years
+        of programming and 3 years of working in high performing agile teams.
       </p>
 
       <p>
-        Outside of programming my interests include writing, journalism, hiking
+        Outside of software my interests include writing, journalism, hiking
         throughout the world, exploring Londons historic pubs, running, reading
         books relating to topics such as sociology, fantasy, politics,
         philosophy and...sensory deprivation tanks ;)
-      </p>
-    </div>
-
-    <div className="about__description-long">
-      <p>
-        I'm a 24 year old full stack software developer based out of Auckland,
-        New Zealand. I currently have 2 years of full stack software development
-        experience within the fintech, aml domain which I attained at AplyId. I
-        gathered this experience after earning a bachelor of engineering in
-        software engineering with first class honours and a variety of
-        internships at companies such as banks, failed start ups and successful
-        start ups.
-      </p>
-
-      <p>
-        You may be wondering, why am I in the technology space? For me
-        technology has acted as a frame through which I live life. It was never
-        a question of "if" I went into the technology industry it was always a
-        question of "what" area of technology I went into. I enjoy software
-        development for its sometimes annoying mix of subjectiveness and
-        objectivness, there isn't always a single right way, there is definitely
-        a wrong way and certainly nicer ways. Figuring out for myself and
-        exploring how others define good code and how to write good code is what
-        I find enjoyable about software development.
-      </p>
-
-      <p>
-        Outside of work my technology interests include functional programming,
-        web3, blockchain and smart contract development i.e. Crypto. Outside of
-        software my interests include hiking throughout the New Zealand native
-        bush, reading various books relating to topics such as sociology,
-        politics, fantasy and philosophy, trying our American and New Zealand
-        craft beers and...sensory deprivation tanks ;)
       </p>
     </div>
   </div>
