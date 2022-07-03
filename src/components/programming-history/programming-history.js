0ab3de2e83a5df1470d225bb1ecfe7a2e1792d49
James Watt <james.watt529@outlook.com>
2022-07-03 12:49:39 +0100

    Partially refactor programming history

--- src/components/programming-history/programming-history.js
+++ src/components/programming-history/programming-history.js
@@ -1,128 +1,60 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './programming-history.css'
 import { PROGRAMMING_EXPERIENCES } from '../../constants/programming-experiences'
 
-const clickOnTab = (e, tabIndex) => {
-  const currentTab = e.currentTarget
-  const tabComponent = currentTab.closest('.tab-component')
-  const id = 'panel-' + tabIndex
-  const currentPanel = document.getElementById(id)
-
-  // 1. Hide current pane and show new one
-  // 2. Move selected class to new tab
-  Array.from(Array(tabs.length).keys()).forEach((element) => {
-    if (
-      document.getElementById('panel-' + element).style.visibility === 'visible'
-    ) {
-      // document.getElementById('panel-' + element).style.display = 'none'
-      document.getElementById('panel-' + element).style.visibility = 'hidden'
-      document.getElementById('panel-' + element).style.position = 'absolute'
-
-      document.getElementById('panel-' + element).style.opacity = '0'
-      // document.getElementById('panel-' + element).style.height = '0'
-      document.getElementById('panel-' + element).style.padding = '0'
-    }
-
-    if (
-      document.getElementById('tab-' + element).style.color ===
-      'var(--body-font-color)'
-    ) {
-      document.getElementById('tab-' + element).style.color =
-        'var(--grey-font-color)'
-      return
-    }
-  })
-
-  currentPanel.style.paddingTop = '10px'
-  currentPanel.style.visibility = 'visible'
-  currentPanel.style.opacity = '1'
-  currentTab.style.color = 'var(--body-font-color)'
-  currentPanel.style.position = 'relative'
-
-  // 3. Update height (maybe width of tab slider)
-  const slider = tabComponent.getElementsByClassName('selected-tab-item')[0]
-  const mediaQuery = window.matchMedia('(max-width: 550px)')
-
-  slider.style.width = currentTab.clientWidth + 'px'
-
-  if (mediaQuery.matches) {
-    slider.style.transform =
-      'translateX(calc(calc(' +
-      tabIndex +
-      '* 120px) + calc(' +
-      tabIndex +
-      '*0px)))'
-  } else {
-    slider.style.transform =
-      'translateY(calc(calc(' +
-      tabIndex +
-      '* var(--tab-height)) + calc(' +
-      tabIndex +
-      '*31.25px)))'
-  }
-}
-
-const tabs = PROGRAMMING_EXPERIENCES.map((element, index) => {
-  return (
-    <button
-      aria-label={element.company + ' work experience'}
-      key={index}
-      className="tab-item"
-      id={'tab-' + index}
-      onClick={(e) => clickOnTab(e, index)}
-    >
-      {element.company}
-    </button>
-  )
-})
-
-const panelBullets = (bullets) => {
-  return bullets.map((element, index) => {
-    return <li key={index}>{element}</li>
-  })
-}
-
-const contentPanels = PROGRAMMING_EXPERIENCES.map((element, index) => {
-  return (
-    <div key={index}>
-      <div className="tab-panel" id={'panel-' + index}>
-        <h3>
-          <span className="company-role">{element.role}</span>
-          <span className="company-name"> @ {element.company}</span>
-        </h3>
-        <p className="date-range">{element.dateRange}</p>
-        <div>
-          <div className="company-description">{element.description}</div>
-          <ul className="experience-points">{panelBullets(element.bullets)}</ul>
-        </div>
-      </div>
-    </div>
-  )
-})
-
-const initialClick = (index) => {
-  const currentPanel = document.getElementById('panel-' + index)
-  const tabComponent = currentPanel.closest('.tab-component')
-  const slider = tabComponent.getElementsByClassName('selected-tab-item')[0]
-  const currentTab = document.getElementById('tab-' + index)
-
-  currentPanel.style.paddingTop = '10px'
-  currentPanel.style.visibility = 'visible'
-  currentPanel.style.opacity = '1'
-  slider.style.width = currentTab.clientWidth + 'px'
-
-  currentTab.style.color = 'var(--body-font-color)'
-
-  const largestPanel = document.getElementById('panel-' + 4)
-  const content = largestPanel.closest('.tab-content')
-  content.style.height = largestPanel.clientHeight + 'px'
-}
-
 const ProgrammingHistory = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const initialClick = (index) => {
+    const currentPanel = document.getElementById('panel-' + index)
+    const tabComponent = currentPanel.closest('.tab-component')
+    const slider = tabComponent.getElementsByClassName('selected-tab-item')[0]
+    const currentTab = document.getElementById('tab-' + index)
+
+    slider.style.width = currentTab.clientWidth + 'px'
+
+    const largestPanel = document.getElementById('panel-' + 4)
+    const content = largestPanel.closest('.tab-content')
+    content.style.height = largestPanel.clientHeight + 'px'
+  }
+
   useEffect(() => {
     initialClick(0)
-  });
+  })
+
+  const clickOnTab = (e, tabIndex) => {
+    const currentPanel = document.getElementById(`panel-${currentIndex}`)
+    const newPanel = document.getElementById(`panel-${tabIndex}`)
+    newPanel.classList.toggle('activated')
+    currentPanel.classList.toggle('activated')
+
+    const newTab = e.currentTarget
+    const currentTab = document.getElementById(`tab-${currentIndex}`)
+    const tabComponent = newTab.closest('.tab-component')
+
+    currentTab.classList.toggle('activated')
+    newTab.classList.toggle('activated')
+
+    const slider = tabComponent.getElementsByClassName('selected-tab-item')[0]
+    const mediaQuery = window.matchMedia('(max-width: 550px)')
+
+    slider.style.width = newTab.clientWidth + 'px'
+
+    if (mediaQuery.matches) {
+      const tabWidthGap = `calc(${tabIndex}*120px)`
+      const paddingGap = `calc(${tabIndex}*0px)`
+      const transformXString = `translateX(calc(${tabWidthGap}} + ${paddingGap}))`
+      slider.style.transform = transformXString
+    } else {
+      const tabHeightGap = `calc(${tabIndex}*31.25px)`
+      const paddingGap = `calc(${tabIndex}* var(--tab-height))`
+      const transformYString = `translateY(calc(${tabHeightGap} + ${paddingGap}))`
+      slider.style.transform = transformYString
+    }
+
+    setCurrentIndex(tabIndex)
+  }
 
   return (
     <div className="programming-history">
@@ -130,13 +62,52 @@ const ProgrammingHistory = () => {
 
       <div className="tab-component">
         <div className="tab-menu">
-          {tabs}
+          {PROGRAMMING_EXPERIENCES.map((element, index) => {
+            return (
+              <button
+                aria-label={element.company}
+                key={index}
+                className={'tab-item ' + (index === 0 ? 'activated' : '')}
+                id={'tab-' + index}
+                onClick={(e) => clickOnTab(e, index)}
+              >
+                {element.company}
+              </button>
+            )
+          })}
 
           <div className="selected-tab-item"></div>
           <div className="menu-border"></div>
         </div>
 
-        <div className="tab-content">{contentPanels}</div>
+        <div className="tab-content">
+          {PROGRAMMING_EXPERIENCES.map((element, index) => {
+            return (
+              <div key={index}>
+                <div
+                  className={'tab-panel ' + (index === 0 ? 'activated' : '')}
+                  id={'panel-' + index}
+                >
+                  <h3>
+                    <span className="company-role">{element.role}</span>
+                    <span className="company-name"> @ {element.company}</span>
+                  </h3>
+                  <p className="date-range">{element.dateRange}</p>
+                  <div>
+                    <div className="company-description">
+                      {element.description}
+                    </div>
+                    <ul className="experience-points">
+                      {element.bullets.map((element, index) => {
+                        return <li key={index}>{element}</li>
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
