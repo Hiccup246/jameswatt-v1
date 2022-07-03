93d483b056f48abd21389957a1dca42b3e896a08
James Watt <james.watt529@outlook.com>
2022-07-03 15:14:43 +0100

    Refactor programming history to use ReactRefs and CSS variables

--- src/components/programming-history/programming-history.js
+++ src/components/programming-history/programming-history.js
@@ -5,105 +5,119 @@ import { PROGRAMMING_EXPERIENCES } from '../../constants/programming-experiences
 
 const ProgrammingHistory = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const tabComponentRef = React.createRef()
+  const sliderComponentRef = React.createRef()
+  const mobileViewWidth = '550px'
 
-  const initialClick = (index) => {
-    const currentPanel = document.getElementById('panel-' + index)
-    const tabComponent = currentPanel.closest('.tab-component')
-    const slider = tabComponent.getElementsByClassName('selected-tab-item')[0]
-    const currentTab = document.getElementById('tab-' + index)
-
-    slider.style.width = currentTab.clientWidth + 'px'
-
-    const largestPanel = document.getElementById('panel-' + 4)
-    const content = largestPanel.closest('.tab-content')
-    content.style.height = largestPanel.clientHeight + 'px'
+  const setupTabs = () => {
+    const newSliderWidth = `${
+      tabComponentRef.current.querySelector(`#tab-${currentIndex}`).clientWidth
+    }px`
+    sliderComponentRef.current.style.width = newSliderWidth
+    tabComponentRef.current.style.height = largestPanelHeight()
   }
 
-  useEffect(() => {
-    initialClick(0)
-  })
+  const largestPanelHeight = () => {
+    let largestPanelHeight = 0
 
-  const clickOnTab = (e, tabIndex) => {
-    const currentPanel = document.getElementById(`panel-${currentIndex}`)
-    const newPanel = document.getElementById(`panel-${tabIndex}`)
+    for (let i = 0; i < PROGRAMMING_EXPERIENCES.length; i++) {
+      const panelHeight = tabComponentRef.current.querySelector(
+        `#panel-${i}`
+      ).clientHeight
+
+      if (panelHeight > largestPanelHeight) {
+        largestPanelHeight = panelHeight
+      }
+    }
+
+    return `${largestPanelHeight}px`
+  }
+
+  const clickOnMenuItem = (element, newTabIndex) => {
+    const currentPanel = tabComponentRef.current.querySelector(
+      `#panel-${currentIndex}`
+    )
+    const newPanel = tabComponentRef.current.querySelector(
+      `#panel-${newTabIndex}`
+    )
+    const currentTab = tabComponentRef.current.querySelector(
+      `#tab-${currentIndex}`
+    )
+    const newTab = element.currentTarget
+
     newPanel.classList.toggle('activated')
     currentPanel.classList.toggle('activated')
-
-    const newTab = e.currentTarget
-    const currentTab = document.getElementById(`tab-${currentIndex}`)
-    const tabComponent = newTab.closest('.tab-component')
 
     currentTab.classList.toggle('activated')
     newTab.classList.toggle('activated')
 
-    const slider = tabComponent.getElementsByClassName('selected-tab-item')[0]
-    const mediaQuery = window.matchMedia('(max-width: 550px)')
+    updateTabMenu(newTabIndex, `${newTab.clientWidth}px`)
+    setCurrentIndex(newTabIndex)
+  }
 
-    slider.style.width = newTab.clientWidth + 'px'
+  const updateTabMenu = (newTabIndex, newSliderWidth) => {
+    const mediaQuery = window.matchMedia(`(max-width: ${mobileViewWidth})`)
+
+    sliderComponentRef.current.style.width = newSliderWidth
 
     if (mediaQuery.matches) {
-      const tabWidthGap = `calc(${tabIndex}*120px)`
-      const paddingGap = `calc(${tabIndex}*0px)`
-      const transformXString = `translateX(calc(${tabWidthGap}} + ${paddingGap}))`
-      slider.style.transform = transformXString
+      const tabWidthGap = `calc(${newTabIndex}*var(--tab-width))`
+      const transformXString = `translateX(${tabWidthGap})`
+      sliderComponentRef.current.style.transform = transformXString
     } else {
-      const tabHeightGap = `calc(${tabIndex}*31.25px)`
-      const paddingGap = `calc(${tabIndex}* var(--tab-height))`
+      const paddingGap = `calc(${newTabIndex}* var(--tab-margin-top))`
+      const tabHeightGap = `calc(${newTabIndex}* var(--tab-height))`
       const transformYString = `translateY(calc(${tabHeightGap} + ${paddingGap}))`
-      slider.style.transform = transformYString
+      sliderComponentRef.current.style.transform = transformYString
     }
-
-    setCurrentIndex(tabIndex)
   }
+
+  useEffect(() => {
+    setupTabs()
+  }, [])
 
   return (
     <div className="programming-history">
       <h2>📜 Programming History</h2>
-
-      <div className="tab-component">
+      <div className="tab-component" ref={tabComponentRef}>
         <div className="tab-menu">
           {PROGRAMMING_EXPERIENCES.map((element, index) => {
             return (
               <button
                 aria-label={element.company}
                 key={index}
-                className={'tab-item ' + (index === 0 ? 'activated' : '')}
-                id={'tab-' + index}
-                onClick={(e) => clickOnTab(e, index)}
+                className={index === 0 ? 'tab-item activated' : 'tab-item'}
+                id={`tab-${index}`}
+                onClick={(element) => clickOnMenuItem(element, index)}
               >
                 {element.company}
               </button>
             )
           })}
 
-          <div className="selected-tab-item"></div>
+          <div ref={sliderComponentRef} className="selected-tab-item"></div>
           <div className="menu-border"></div>
         </div>
 
         <div className="tab-content">
           {PROGRAMMING_EXPERIENCES.map((element, index) => {
             return (
-              <div key={index}>
-                <div
-                  className={'tab-panel ' + (index === 0 ? 'activated' : '')}
-                  id={'panel-' + index}
-                >
-                  <h3>
-                    <span className="company-role">{element.role}</span>
-                    <span className="company-name"> @ {element.company}</span>
-                  </h3>
-                  <p className="date-range">{element.dateRange}</p>
-                  <div>
-                    <div className="company-description">
-                      {element.description}
-                    </div>
-                    <ul className="experience-points">
-                      {element.bullets.map((element, index) => {
-                        return <li key={index}>{element}</li>
-                      })}
-                    </ul>
-                  </div>
-                </div>
+              <div
+                key={index}
+                className={index === 0 ? 'tab-panel activated' : 'tab-panel'}
+                id={`panel-${index}`}
+              >
+                <h3>
+                  <span className="company-role">{element.role}</span>
+                  <span className="company-name"> @ {element.company}</span>
+                </h3>
+                <p className="date-range">{element.dateRange}</p>
+                <div className="company-description">{element.description}</div>
+                <ul className="experience-points">
+                  {element.bullets.map((element, index) => {
+                    return <li key={index}>{element}</li>
+                  })}
+                </ul>
               </div>
             )
           })}
